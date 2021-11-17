f534d4a3b6073b180d607648ef20c2e770cb546b
Crutoi Alexandru <crutoi2001@gmail.com>
2021-11-17 15:19:25 +0200

    Create wishlist commands

--- discord_bot/on_message.py
+++ discord_bot/on_message.py
@@ -1,12 +1,56 @@
 import discord 
+from discord.ext import commands
+import filelist.wishlist as wishlist
 
+# list of commands for our bot
+help = '''
+    These are my commands: 
+    fl.run  - start my data feed
+    fl.wl   - add a torrent to wishlist
+    fl.rm   - remove a torrent from the wishlist
+    fl.show - show the wishlist items
+    '''
+
+# listen for messages from users
 def on_message(client):
-    @client.event
-    async def on_ready():
-        print("Logged in")
-
+    # commands to do on certain receives messages
     @client.event
     async def on_message(message):
+        # ignore our own bot's chat
         if message.author == client.user: return 
 
+        # ping command for testing if bot is alive
         if message.content.startswith("ping"): await message.channel.send("pong")
+
+        # command to se our bot help guide
+        if message.content.startswith("fl.help"): await message.channel.send(help)
+
+        # wishlist command
+        # add to our wishlist list(wishlist) of torrents
+        if message.content.startswith("fl.wl"): 
+            # isolate only the torrent to be added
+            torrent = message.content.replace("fl.wl", "")
+            torrent = torrent.replace(" ", "")
+
+            wishlist.wishlist(torrent)
+
+            # send a message to confirm the torrent name
+            await message.channel.send("Added to wishlist: " + torrent)
+
+        # remove command
+        # remove from our wishlist list(wishlist) of torrents
+        if message.content.startswith("fl.rm"): 
+            # isolate only the torrent to be added
+            torrent = message.content.replace("fl.rm", "")
+            torrent = torrent.replace(" ", "")
+
+            wishlist.remove(torrent)
+
+            # send a message to confirm the torrent name
+            await message.channel.send("Removed from wishlist: " + torrent)
+
+        # show command
+        # show our wishlist items
+        if message.content.startswith("fl.show"): 
+            # send a message to show wishlist
+            await message.channel.send("This is your wishlist:\n" + wishlist.show())
---
+++ filelist/wishlist.py
@@ -0,0 +1,86 @@
+import csv
+from os.path import exists
+
+# check if the csv files exists otherwise create it
+def is_wishlist():
+    if not exists("wishlist.csv"):
+        with open("wishlist.csv", "w") as file:
+            torrent_writer = csv.writer(file)
+
+# function adds a non empty string to a list of strings in a csv
+def wishlist(torrent):
+    is_wishlist()
+    torrent_wishlist = []
+    index = 0
+
+    # if the input is empty ignore it
+    if torrent == "": return
+
+    #  open the csv file
+    with open("wishlist.csv") as file:
+        torrent_reader = csv.reader(file)
+
+        # write every string in the csv into our list
+        torrent_wishlist = list(torrent_reader)
+
+    # ignore torrent if already in wishlist
+    while index < len(torrent_wishlist):
+        # remove all non torrent data from csv
+        # could not find a good way to write clean data to csv
+        torrent_string = str(torrent_wishlist[index]).replace(",", "").replace(" ", "").replace("[", "").replace("]", "").replace("'", "")
+        if torrent == torrent_string: return
+        index += 1
+
+    # insert our torrent at the end of the list
+    torrent_wishlist.append(torrent)
+
+    # update the csv file with our list
+    with open("wishlist.csv", "w") as file:
+        torrent_writer = csv.writer(file)
+        torrent_writer.writerows(torrent_wishlist)
+
+# function removes a non empty string from a list of string in a csv
+def remove(torrent):
+    is_wishlist()
+    torrent_wishlist = []
+    update_torrent_wishlist = []
+    index = 0
+
+    # if the input is empty ignore it
+    if torrent == "": return
+
+    #  open the csv file
+    with open("wishlist.csv") as file:
+        torrent_reader = csv.reader(file)
+
+        # write every string in the csv into our list
+        torrent_wishlist = list(torrent_reader)
+
+    # remove torrent from wishlist by ignoring it in the reading
+    while index < len(torrent_wishlist):
+        # remove all non torrent data from csv
+        # could not find a good way to write clean data to csv
+        torrent_string = str(torrent_wishlist[index]).replace(",", "").replace(" ", "").replace("[", "").replace("]", "").replace("'", "")
+        if torrent != torrent_string: update_torrent_wishlist.append(torrent_wishlist[index])
+        index += 1
+
+    # update the csv file with our list
+    with open("wishlist.csv", "w") as file:
+        torrent_writer = csv.writer(file)
+        torrent_writer.writerows(update_torrent_wishlist)
+
+# function shows to user all the items in the wishlist
+def show():
+    is_wishlist()
+    torrent_wishlist = []
+
+    #  open the csv file
+    with open("wishlist.csv") as file:
+        torrent_reader = csv.reader(file)
+
+        # write every string in the csv into our list
+        torrent_wishlist = list(torrent_reader)
+    
+    # return our csv list cleanly
+    torrent_string = str(torrent_wishlist).replace(",", "").replace(" ", "").replace("[", "").replace("]", "\n").replace("'", "")
+    return torrent_string

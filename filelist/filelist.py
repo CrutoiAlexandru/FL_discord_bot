99c1ef16aede3136bf51307e5650f061bf15a01b
Crutoi Alexandru <45456122+CrutoiAlexandru@users.noreply.github.com>
2021-11-18 10:27:58 +0200

    Update filelist.py

--- filelist/filelist.py
+++ filelist/filelist.py
@@ -1,13 +1,8 @@
-# import requests
-# from requests.auth import HTTPBasicAuth
 import feedparser
 import config
 
-def get_rss_feed():
-    feed = feedparser.parse("https://filelist.io/rss.php?username=" + config.USERNAME + "&passkey=" + config.PASSKEY)
-
-    print(feed.entries[0].title)
-    return feed.entries[0].title
-
 def run():
-    return get_rss_feed()
+    # aquires the rss feed from filelist, needs login
+    feed = feedparser.parse("https://filelist.io/rss.php?username=" + config.USERNAME + "&passkey=" + config.PASSKEY)
+    #return feed's latest torrent title
+    return feed.entries[0].title
